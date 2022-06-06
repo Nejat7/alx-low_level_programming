f4849677ee8dd767b750dca48704887de3751c5e
Nejat Dedgeba <nejatdoc@gmail.com>
2022-06-06 08:23:20 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * use the function printf
+ * you are not allowed to use the function puts
+ * your program should return 0
+ * your program should compile without warning when using the -Wall gcc
+ * gcc option 
+ *
+ * return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
+
