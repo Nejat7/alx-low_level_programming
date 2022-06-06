d7bab3b0bb1a809e8b80f33bc3af1bef98658d24
Nejat Dedgeba <nejatdoc@gmail.com>
2022-06-06 01:24:30 -0700

    task 3, task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * use the function puts.
+ * not allowed to use printf.
+ * should end with the value 0
+ *
+ * return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
