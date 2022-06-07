1ef72346e55dc8ae20e8160ab650deb12b4274af
Nejat Dedgeba <nejatdoc@gmail.com>
2022-06-07 13:04:26 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *Return: Always 0
+ */
+void print_alphabet(void);
+{
+char alp = 'a';
+while (alp <= 'z')
+{
+_putchar (alp);
+alp++;
+}
+_putchar ('/n);
+}
