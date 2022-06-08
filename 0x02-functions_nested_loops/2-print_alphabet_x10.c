ae9c034239a19b8250fe59f748a067f74dd5fb7f
Nejat Dedgeba <nejatdoc@gmail.com>
2022-06-07 23:22:41 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - Entry point
+*ten times
+*/
+void print_alphabet_x10(void);
+{
+int alp;
+int x;
+for (x = 0; x < 10; x++)
+{
+for (alp = 'a'; alp <= 'z'; alp++)
+{
+_putchar(alp);
+}
+-putchar('\n');
+}
+}
