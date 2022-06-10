aaa181fca8757c0c987ff08b41cc8eb54c521f22
Nejat Dedgeba <nejatdoc@gmail.com>
2022-06-09 23:24:19 -0700

    tasks

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,28 +1,27 @@
-#include "main.h"
+#include <stdio.h>
 /**
- *print_triangle - prints a triangle
- *@size:size parameter of triangle
- *Return:returns nothing
- */
-void print_triangle(int size)
+*main- prints the largest prime factor
+*of a number
+*
+*Return: returns 0
+*/
+int main(void)
 {
-	int inc1, inc2;
-
-	if (size > 0)
-	{
-		for (inc1 = 1; inc1 <= size; inc1++)
-		{
-			for ((inc2 = size - inc1); inc2 > 0; inc2--)
-				_putchar(' ');
-
-			for (inc2 = 0; inc2 < inc1; inc2++)
-				_putchar('#');
-
-			if (inc1 == size)
-				continue;
-
-			_putchar('\n');
-		}
-	}
-	_putchar('\n');
+long number = 612852475143;
+int inc;
+while (inc++ < number / 2)
+{
+if (number % inc == 0)
+{
+number /= 2;
+continue;
+}
+for (inc = 3; inc < number / 2; inc += 2)
+{
+if (number % inc == 0)
+number /= inc;
+}
+}
+printf("%ld\n", number);
+return (0);
 }
