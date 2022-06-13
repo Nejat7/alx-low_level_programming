af1dd88c270c89f086c3d29c5797c9a228e7f20f
Nejat Dedgeba <nejatdoc@gmail.com>
2022-06-13 01:05:29 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *reset_to_98 - reset value of am interger to 98
+ *@n: Parameter targeted
+ *Return : nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifnder MAIN_H
+#define MAIN_H
+#include <stdiio.h>
+
+/**
+*void prototype()
+*char prototype()
+*int prototype()
+*/
+
+void reset_to_98(int *n);
+void _puts(char *str);
+
+#endif /*MAIN_H*/
